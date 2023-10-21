439fe5e3e7b186d769128fa272459d1d69d301e0
Taiveyon Shaw <taiveyonshaw@gmail.com>
2023-10-21 15:35:46 -0700

    Update

--- components/main.js
+++ components/main.js
@@ -119,11 +119,12 @@ function scrollActive(){
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
+
 window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
